a09273641dbf645a13451615f3074695761ca5ad
Momfer de Mol <93478412+momferdemol@users.noreply.github.com>
2025-05-29 13:15:16 +0200

    update file share

--- lxc-media/configuration.nix
+++ lxc-media/configuration.nix
@@ -101,7 +101,9 @@
     "/mnt/media" = {
       device = "//192.168.10.26/Media";
       fsType = "cifs";
-      options = [ "credentials=/var/lib/jellyfin/.synology" "rw" "noperm" "uid=100000" ];
+      options = let
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      in ["${automount_opts},credentials=/etc/nixos/.synology"];
     };
   };
 
